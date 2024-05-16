409aae59aa19799ee5695613a8ea0dcc3484227c
hyunsoo730 <whgustn730@gmail.com>
2024-05-16 10:41:29 +0900

    Feat : XML 파싱

--- BE/final-project/src/main/java/com/ssafy/finalproject/aptsale/dto/request/AptSaleDTO.java
+++ BE/final-project/src/main/java/com/ssafy/finalproject/aptsale/dto/request/AptSaleDTO.java
@@ -1,15 +1,11 @@
 package com.ssafy.finalproject.aptsale.dto.request;
 
-import jakarta.xml.bind.annotation.XmlAccessType;
-import jakarta.xml.bind.annotation.XmlAccessorType;
-import jakarta.xml.bind.annotation.XmlElement;
-import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.*;
 import lombok.*;
 
 import java.util.List;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -17,89 +13,84 @@ import java.util.List;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class AptSaleDTO {
 
+    @XmlElement(name = "header")
+    private Header header;
+
     @XmlElement(name = "body")
     private Body body;
 
-    private String aptCode; // ! processor에서 가공한 코드
-
-    public void setAptCode() {
-        Item item = body.getItems().getItemList().get(0);
-        this.aptCode = item.getLegalDongSigunguCode() + item.getLegalDongEubmyeondongCode()
-                + item.getRoadNameBonbun() + item.getRoadNameBubun();
-    }
-
-    @XmlRootElement(name = "body")
-    @XmlAccessorType(XmlAccessType.FIELD)
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
-    private static class Body {
+    @XmlRootElement(name = "header")
+    @XmlAccessorType(XmlAccessType.FIELD)
+    public static class Header {
+        @XmlElement(name = "resultCode")
+        private String resultCode;
+        @XmlElement(name = "resultMsg")
+        private String resultMsg;
+    }
+
+    @Data
+    @XmlRootElement(name = "body")
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Body {
         @XmlElement(name = "items")
         private Items items;
     }
 
+    @Data
     @XmlRootElement(name = "items")
     @XmlAccessorType(XmlAccessType.FIELD)
-    @Data
     @NoArgsConstructor
     @AllArgsConstructor
-    private static class Items {
+    public static class Items {
         @XmlElement(name = "item")
         private List<Item> itemList;
     }
 
+    @Data
     @XmlRootElement(name = "item")
     @XmlAccessorType(XmlAccessType.FIELD)
-    @Data
     @NoArgsConstructor
     @AllArgsConstructor
-    private static class Item {
+    public static class Item {
         @XmlElement(name = "거래금액")
         private Integer dealAmount;
-
         @XmlElement(name = "건축년도")
         private Integer constructionYear;
-
         @XmlElement(name = "도로명")
         private String roadName;
-
         @XmlElement(name = "도로명건물본번호코드")
         private String roadNameBonbun;
-
         @XmlElement(name = "도로명건물부번호코드")
         private String roadNameBubun;
-
         @XmlElement(name = "도로명시군구코드")
         private String roadNameSigunguCode;
-
         @XmlElement(name = "동")
         private String dong;
-
         @XmlElement(name = "법정동")
         private String legalDong;
-
         @XmlElement(name = "법정동시군구코드")
         private String legalDongSigunguCode;
-
         @XmlElement(name = "법정동읍면동코드")
         private String legalDongEubmyeondongCode;
-
         @XmlElement(name = "아파트")
         private String aptName;
-
         @XmlElement(name = "년")
         private Integer dealYear;
-
         @XmlElement(name = "월")
         private Integer dealMonth;
-
         @XmlElement(name = "일")
         private Integer dealDay;
-
         @XmlElement(name = "전용면적")
         private Double exclusiveArea;
-
         @XmlElement(name = "층")
         private Integer floor;
+
+        @XmlTransient
+        private String aptCode;
     }
 }
--- BE/final-project/src/main/java/com/ssafy/finalproject/config/batch/AptTradeDetailBatchJob.java
+++ BE/final-project/src/main/java/com/ssafy/finalproject/config/batch/AptTradeDetailBatchJob.java
@@ -9,16 +9,18 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.*;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -28,7 +30,7 @@ public class AptTradeDetailBatchJob {
     private final WebClient webClient;
     private final PlatformTransactionManager transactionManager;
     private static final String SEOUL_BASE_URL = "http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev";
-    private static final String ENCODED_API_KEY = "6hQ6uOLRUTGDB5xRbXb9ELHp1xs%2FBzRVXfyaMvHOySW1fS4OYabUvqUXaiuFdsXV2Yj000Ja53eGvkZGYVnDqw%3D%3D";
+    private static final String ENCODED_API_KEY = "jMJCTIZSOT9a34dKu0QVnzV4psUhzFnra60JbkjydQelnAYlftbQs1tTrEMN3AmKxjhChrcKi9eaBUGwirsXGw%3D%3D";
     private static final String SEOUL_API_KEY = URLDecoder.decode(ENCODED_API_KEY, StandardCharsets.UTF_8);
     //    private static final String API_KEY = URLDecoder.decode(ENCODED_API_KEY, StandardCharsets.UTF_8);
 
@@ -45,132 +47,63 @@ public class AptTradeDetailBatchJob {
     @Bean
     public Step aptTradeDetailXmlParseingStep(JobRepository jobRepository){
         return new StepBuilder("aptTradeDetailXmlParsingStep", jobRepository)
-                .<String, String>chunk(10, transactionManager)  // ! String으로 읽어와서(Read) String으로 쓰겠다(Writer)
-//                .<AptSaleDTO, AptSaleDTO>chunk(10, transactionManager)
+                .<AptSaleDTO, AptSaleDTO>chunk(1000, transactionManager)
                 .reader(aptTradeDetailReader())
-//                .processor(aptTradeDetailProcessor()) // ! 현재 읽어오기만 한거라 processor 필요없음
+                .processor(aptTradeDetailProcessor()) // ! 현재 읽어오기만 한거라 processor 필요없음
                 .writer(aptTradeDetailWriter())
                 .build();
     }
 
-//    @Bean
-//    public Step aptTradeDetailStep(JobRepository jobRepository) {
-//        return new StepBuilder("aptTradeDetailStep", jobRepository)
-//                .<String, String>chunk(10, transactionManager)
-//                .reader(aptTradeDetailReader())
-//                .processor(aptTradeDetailProcessor())
-//                .writer(aptTradeDetailWriter())
-//                .build();
-//    }
 
     // ! XML로 파싱해서 읽어올 부분, WebClient는 OPEN API 호출할 때마다 URL 수정해야함 (mutate)
-//    @Bean
-//    public ItemReader<AptSaleDTO> aptTradeDetailReader() {
-//        return () -> {
-//            AptSaleDTO response = webClient.mutate()
-//                    .baseUrl(SEOUL_BASE_URL)
-//                    .build()
-//                    .get().uri(uriBuilder ->
-//                            uriBuilder.queryParam("serviceKey", SEOUL_API_KEY)
-//                                    .queryParam("pageNo", "1")
-//                                    .queryParam("numOfRows", "10")
-//                                    .queryParam("LAWD_CD", "11110")
-//                                    .queryParam("DEAL_YMD", "201512")
-//                                    .build())
-//                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE)
-//                    .retrieve()
-//                    .bodyToMono(AptSaleDTO.class)
-//                    .blockOptional().orElse(null); // block 대신 blockOptional 사용하여 null 처리
-////            String decodedApiKey = URLDecoder.decode(ENCODED_API_KEY, StandardCharsets.UTF_8);
-////
-////            String response = webClient.get()
-////                    .uri(uriBuilder ->
-////                            uriBuilder.queryParam("serviceKey", decodedApiKey)
-////                                    .queryParam("pageNo", "1")
-////                                    .queryParam("numOfRows", "10")
-////                                    .queryParam("LAWD_CD", "11110")
-////                                    .queryParam("DEAL_YMD", "201512")
-////                                    .build())
-////                    .retrieve()
-////                    .bodyToMono(String.class)
-////                    .blockOptional().orElse(""); // block 대신 blockOptional 사용하여 null 처리
-//////                    .block();
-//
-//            return response;
-//        };
-//    }
-
     @Bean
-    public ItemReader<String> aptTradeDetailReader() {
+    public ItemReader<AptSaleDTO> aptTradeDetailReader() {
         return () -> {
-//            AptSaleDTO response = webClient.mutate()
-//                    .baseUrl(SEOUL_BASE_URL)
-//                    .build()
-//                    .get().uri(uriBuilder ->
-//                            uriBuilder.queryParam("serviceKey", SEOUL_API_KEY)
-//                                    .queryParam("pageNo", "1")
-//                                    .queryParam("numOfRows", "10")
-//                                    .queryParam("LAWD_CD", "11110")
-//                                    .queryParam("DEAL_YMD", "201512")
-//                                    .build())
-//                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE)
-//                    .retrieve()
-//                    .bodyToMono(AptSaleDTO.class)
-//                    .blockOptional().orElse(null); // block 대신 blockOptional 사용하여 null 처리
-            String decodedApiKey = URLDecoder.decode(ENCODED_API_KEY, StandardCharsets.UTF_8);
-
-            String response = webClient.mutate()
-                            .baseUrl(SEOUL_BASE_URL)
-                                    .build()
-                                            .get().uri(uriBuilder ->
-                            uriBuilder.queryParam("serviceKey", decodedApiKey)
+            AptSaleDTO response = webClient.mutate()
+                    .baseUrl(SEOUL_BASE_URL)
+                    .build()
+                    .get().uri(uriBuilder ->
+                            uriBuilder.queryParam("serviceKey", SEOUL_API_KEY)
                                     .queryParam("pageNo", "1")
-                                    .queryParam("numOfRows", "10")
+                                    .queryParam("numOfRows", "1")
                                     .queryParam("LAWD_CD", "11110")
                                     .queryParam("DEAL_YMD", "201512")
                                     .build())
+//                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE)
+                    .accept(MediaType.valueOf(MediaType.APPLICATION_XML_VALUE))
                     .retrieve()
-                    .bodyToMono(String.class) // ! String 매핑
-                    .blockOptional().orElse(""); // block 대신 blockOptional 사용하여 빈 문자열 처리
+                    .bodyToMono(AptSaleDTO.class)
+                    .block();
+//                    .blockOptional().orElse(null); // block 대신 blockOptional 사용하여 null 처리
+
             return response;
         };
     }
 
-    // ! XML 파싱 후 aptCode 가공을 위해 -> ItemProcessor
-    //    @Bean
-//    public ItemProcessor<AptSaleDTO, AptSaleDTO> aptTradeDetailProcessor() {
-//        return item -> {
-//            // 여기서 이제 aptCode 가공 후 삽입 진행 : 이 부분을 수행해주세요
-//            item.setAptCode();
-//            return item;
-//        };
-//    }
 
-    @Bean
+    // ! XML 파싱 후 aptCode 가공을 위해 -> ItemProcessor
+        @Bean
     public ItemProcessor<AptSaleDTO, AptSaleDTO> aptTradeDetailProcessor() {
-        return item -> {
-            // 여기서 이제 aptCode 가공 후 삽입 진행 : 이 부분을 수행해주세요
-            item.setAptCode();
-            return item;
+        return aptSaleDTO -> {
+//            List<AptSaleDTO.Item> itemList = aptSaleDTO.getBody().getItems().getItemList();
+//            itemList.forEach(item -> {
+//                String aptCode = item.getLegalDongSigunguCode() + item.getLegalDongEubmyeondongCode()
+//                        + item.getRoadNameBonbun() + item.getRoadNameBubun();
+////                item.setAptCode(aptCode);
+//            });
+            return aptSaleDTO;
         };
     }
 
-    // ! XML 파싱 후 제대로 확인하기 위해 ItemReader : 현재 미정
-    //    @Bean
-//    public ItemWriter<AptSaleDTO> aptTradeDetailWriter() {
-//        return items -> {
-//            for (AptSaleDTO item : items) {
-//                log.info("AptSaleDTO = {}", item);
-//            }
-//        };
-//    }
 
-    @Bean
-    public ItemWriter<String> aptTradeDetailWriter() {
+    // ! XML 파싱 후 제대로 확인하기 위해 ItemReader : 현재 미정
+        @Bean
+    public ItemWriter<AptSaleDTO> aptTradeDetailWriter() {
         return items -> {
-            for (String item : items) {
-                log.info("AptSaleDTO = {}", item);
+            for (AptSaleDTO item : items) {
+                log.info("AptSaleDTO = {}", item.toString());
             }
         };
     }
+
 }
