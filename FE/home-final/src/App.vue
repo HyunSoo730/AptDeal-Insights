1250f7d6a483a62fb9e744cdfe83af2a8dd8d566
FinancialWorld16 <rhgksrkd@naver.com>
2024-05-16 22:20:57 +0900

    copy master FE folder

--- FE/home-final/src/App.vue
+++ FE/home-final/src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-import Kakao from "@/components/Kakao.vue";
-import HelloWorld from '@/components/HelloWorld.vue';
-
+import TheHeader from "@/components/TheHeader.vue";
+import TheNavBar from "@/components/TheNavBar.vue";
+import TheFooter from "@/components/TheFooter.vue";
+import TheSearch from "@/components/TheSearch.vue";
 </script>
 
 <template>
-  <div>
-    추가7777
-    <HelloWorld></HelloWorld>
-    <Kakao></Kakao>
+  <div class="flex flex-col min-h-screen">
+    <TheHeader />
+    <TheNavBar />
+    <TheSearch />
+    <router-view></router-view>
+    <TheFooter />
   </div>
 </template>
-
-<style scoped></style>
---
+++ FE/home-final/src/components/TheNavBar.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+    <nav class="bg-gray-100">
+      <div class="container mx-auto">
+        <ul class="flex justify-center space-x-8 py-4">
+          <li><a href="#" class="text-gray-700 hover:text-black text-lg">매매</a></li>
+          <li><a href="#" class="text-gray-700 hover:text-black text-lg">전월세</a></li>
+          <li><a href="#" class="text-gray-700 hover:text-black text-lg">신축분양</a></li>
+          <li><a href="#" class="text-gray-700 hover:text-black text-lg">인구흐름</a></li>
+        </ul>
+      </div>
+    </nav>
+  </template>
+
+<style scoped>
+
+</style>
