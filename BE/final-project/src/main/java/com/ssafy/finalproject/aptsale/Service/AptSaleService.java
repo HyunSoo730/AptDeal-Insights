95500870f4aaadf6141965e5bcd77fa29228c9fe
FinancialWorld16 <rhgksrkd@naver.com>
2024-05-17 16:44:18 +0900

    Feat: open api에서 drop down 불러오기 완료

--- BE/final-project/src/main/java/com/ssafy/finalproject/aptsale/Service/AptSaleService.java
+++ BE/final-project/src/main/java/com/ssafy/finalproject/aptsale/Service/AptSaleService.java
@@ -1,4 +1,23 @@
 package com.ssafy.finalproject.aptsale.Service;
 
+
+import com.ssafy.finalproject.aptsale.entity.AptSale;
+import com.ssafy.finalproject.aptsale.repository.AptSaleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
 public class AptSaleService {
+
+    private final AptSaleRepository aptSaleRepository;
+
+
+    public List<AptSale> getApartmentsByDongCode(String dongcode) {
+
+        return aptSaleRepository.findByDongCodeGroupByAptCode(dongcode);
+    }
 }
