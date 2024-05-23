cd13468f6777f8c398506ea9dc496debd3720bc0
hyunsoo <whgustn730@gmail.com>
2024-05-23 15:21:00 +0900

    Feat: 전월세 디자인 및 레이아웃 수정

--- FE/home-final/src/components/TheNavBar.vue
+++ FE/home-final/src/components/TheNavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="bg-teal-600 shadow-lg mt-4">
     <div class="container mx-auto py-4">
-      <ul class="flex justify-center space-x-8">
+      <ul class="flex justify-center space-x-12">
         <li><router-link to="/" class="text-white hover:text-teal-200 text-lg">매매</router-link></li>
         <li><router-link to="/lease" class="text-white hover:text-teal-200 text-lg">전월세</router-link></li>
         <li><router-link to="/new-construction" class="text-white hover:text-teal-200 text-lg">신축분양</router-link></li>
