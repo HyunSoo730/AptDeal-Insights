17d4406fc9ae8eaa97db851d7449e9c4c6f93112
hyunsoo <whgustn730@gmail.com>
2024-05-23 17:21:12 +0900

    Feat: 백엔드 아파트 동 조회 페이징 처리

--- BE/final-project/src/main/java/com/ssafy/finalproject/aptsale/dto/request/AptSaleByDongDTO.java
+++ BE/final-project/src/main/java/com/ssafy/finalproject/aptsale/dto/request/AptSaleByDongDTO.java
@@ -1,5 +1,6 @@
 package com.ssafy.finalproject.aptsale.dto.request;
 
+import com.querydsl.core.annotations.QueryProjection;
 import com.ssafy.finalproject.like.entity.LikeApt;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -10,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class AptSaleByDongDTO {
     private String aptName;
     private Integer dealAmount;
@@ -19,4 +19,15 @@ public class AptSaleByDongDTO {
     private String aptCode;
     private Double latitude;
     private Double longitude;
+
+    @QueryProjection
+    public AptSaleByDongDTO(String aptName, Integer dealAmount, String roadName, Integer constructionYear, String aptCode, Double latitude, Double longitude) {
+        this.aptName = aptName;
+        this.dealAmount = dealAmount;
+        this.roadName = roadName;
+        this.constructionYear = constructionYear;
+        this.aptCode = aptCode;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
